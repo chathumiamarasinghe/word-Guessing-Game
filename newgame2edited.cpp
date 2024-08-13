b937b74eb68d45070c4103667a8dad0af7fbe90f
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-08-13 15:24:47 +0530

    add input validation & dynamic word laoding

--- newgame2edited.cpp
+++ newgame2edited.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>  
+#include <vector>   
 
 using namespace std;
 
@@ -12,7 +14,14 @@ string chooseRandomWord(const string words[], int numWords) {
     return words[randomIndex];
 }
 
-// Function to display the current state of the word with guessed letters
+// Function to choose a random word from a vector
+string chooseRandomWordFromVector(const vector<string>& words) {
+    srand(static_cast<unsigned int>(time(0)));
+    int randomIndex = rand() % words.size();
+    return words[randomIndex];
+}
+
+
 void displayWord(const string& word, const bool guessed[], int wordLength) {
     for (int i = 0; i < wordLength; i++) {
         if (guessed[i]) {
@@ -25,7 +34,8 @@ void displayWord(const string& word, const bool guessed[], int wordLength) {
 }
 
 void displayHangman(int attempts) {
-     switch (attempts) {
+   
+    switch (attempts) {
         case 0:
             cout << "_________" << endl;
             cout << "|       |" << endl;
@@ -85,56 +95,88 @@ void displayHangman(int attempts) {
     }
 }
 
+
+vector<string> loadWordsFromFile(const string& filename) {
+    vector<string> words;
+    ifstream file(filename.c_str());  // Convert std::string to const char* using c_str()
+    string word;
+    while (file >> word) {
+        words.push_back(word);
+    }
+    return words;
+}
+
+
+string provideHint(const string& word) {
+    
+    return string(1, word[0]);
+}
+
+
+char getValidatedInput() {
+    char input;
+    while (true) {
+        cin >> input;
+        if (isalpha(input)) {
+            input = tolower(input);  // Convert to lowercase
+            break;
+        } else {
+            cout << "Invalid input. Please enter a letter: ";
+        }
+    }
+    return input;
+}
+
 int main() {
     const int maxAttempts = 6;
-    const int numWords = 10;
-    const int maxWordLength = 20; // Maximum word length
-    string block;
     int score = 0;
 
-    string word_fruit[] = {"apple", "banana", "cherry", "grape", "orange", "avocado", "papaya", "strawberry", "mango", "guava","kiwi","pineapple","watermelon","Pomegranate","blueberry"};
-    string word_animal[] = {"cat", "horse", "dog", "snake", "rabbit", "elephant", "Jaguar", "Panda", "scorpion", "sheep","Shark","Zebra","Gorilla","Leopard","Kangaroo"};
-    string word_vehicle[] = {"boat", "bicycle", "ship", "tractor", "plane", "truck", "excavator", "ambulance", "helicopter", "scooter","spaceshuttle","skateboard","Crane","Airplane","tram"};
-    string word_Movie[]={"titanic","jurassic","inception","tenet","oppenheimer"," interstellar","Avatar","Matrix","Gravity","Gladiator"};
-    string word_Planet[]={"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto","Eris"};
-    string word_country[]={"usa","canada","france","japan","india","brazil","australia","russia","germany","china","newzealand","maldives","nepal","pakistan","finland"};
+    // Word categories
+    string word_fruit[] = {"apple", "banana", "cherry", "grape", "orange", "avocado", "papaya", "strawberry", "mango", "guava"};
+    string word_animal[] = {"cat", "horse", "dog", "snake", "rabbit", "elephant", "Jaguar", "Panda", "scorpion", "sheep"};
+    string word_vehicle[] = {"boat", "bicycle", "ship", "tractor", "plane", "truck", "excavator", "ambulance", "helicopter", "scooter"};
+    string word_movie[] = {"titanic", "jurassic", "inception", "tenet", "oppenheimer", "interstellar", "avatar", "matrix", "gravity", "gladiator"};
+    string word_planet[] = {"mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto", "eris"};
+    string word_country[] = {"usa", "canada", "france", "japan", "india", "brazil", "australia", "russia", "germany", "china"};
 
-    string word_medium[]={"apple", "banana", "cherry", "grape", "orange", "avocado", "papaya", "strawberry", "mango", "guava","kiwi","pineapple","watermelon",
-	"Pomegranate","blueberry","cat", "horse", "dog", "snake", "rabbit", "elephant", "Jaguar", "Panda", "scorpion", "sheep","Shark","Zebra","Gorilla","Leopard","Kangaroo",
-	"boat", "bicycle", "ship", "tractor", "plane", "truck", "excavator", "ambulance", "helicopter", "scooter","spaceshuttle","skateboard","Crane","Airplane","tram",
-	"titanic","jurassic","inception","tenet","oppenheimer"," interstellar","Avatar","Matrix","Gravity","Gladiator","Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto","Eris",
-	"usa","canada","france","japan","india","brazil","australia","russia","germany","china","newzealand","maldives","nepal","pakistan","finland"};
+    vector<string> dynamic_words = loadWordsFromFile("words.txt");  // Example of dynamic word list
+
     string wordToGuess;
-    bool guessed[maxWordLength];
-    
-    string game_level;
-    
-
-    cout << "Welcome to the word master Game!" << endl;
-    cout << "Guess the word" << endl;
-    cout<<"select game level(easy/medium/hard)"<<endl;
-    cin>>game_level;
-    
-
+    bool guessed[20];
+    string game_level, category;
     char playAgain;
-   if(game_level=="easy"){
-   
+
+    cout << "Welcome to the Word Master Game!" << endl;
+    cout << "Guess the word" << endl;
+    cout << "Select game level (easy/medium/hard): ";
+    cin >> game_level;
+
     do {
-        cout << "Choose your field (fruit/animal/vehicle/movie/planet): ";
-        cin >> block;
+        if (game_level == "easy") {
+            cout << "Choose your field (fruit/animal/vehicle/movie/planet/country): ";
+            cin >> category;
 
-        if (block == "fruit") {
-            wordToGuess = chooseRandomWord(word_fruit, numWords);
-        } else if (block == "animal") {
-            wordToGuess = chooseRandomWord(word_animal, numWords);
-        } else if (block == "vehicle") {
-            wordToGuess = chooseRandomWord(word_vehicle, numWords);
-        } else if (block == "movie") {
-            wordToGuess = chooseRandomWord(word_Movie, numWords);
-        } else if (block == "planet") {
-            wordToGuess = chooseRandomWord(word_Planet, numWords);
-        } else if (block == "country") {
-            wordToGuess = chooseRandomWord(word_country, numWords);
+            if (category == "fruit") {
+                wordToGuess = chooseRandomWord(word_fruit, 10);
+            } else if (category == "animal") {
+                wordToGuess = chooseRandomWord(word_animal, 10);
+            } else if (category == "vehicle") {
+                wordToGuess = chooseRandomWord(word_vehicle, 10);
+            } else if (category == "movie") {
+                wordToGuess = chooseRandomWord(word_movie, 10);
+            } else if (category == "planet") {
+                wordToGuess = chooseRandomWord(word_planet, 10);
+            } else if (category == "country") {
+                wordToGuess = chooseRandomWord(word_country, 10);
+            }
+
+        } else if (game_level == "medium") {
+            
+            wordToGuess = chooseRandomWordFromVector(dynamic_words);
+        } else if (game_level == "hard") {
+            
+            vector<string> hard_words = loadWordsFromFile("hard_words.txt");
+            wordToGuess = chooseRandomWordFromVector(hard_words);
         }
 
         int wordLength = wordToGuess.length();
@@ -149,30 +191,7 @@ int main() {
             displayHangman(attempts);
             displayWord(wordToGuess, guessed, wordLength);
 
-            char guess;
-            cout << "Guess a letter";
-
-            if (attempts == maxAttempts - 1) {
-                cout << " or enter 'R' for a reset power-up or 'L' for a letter reveal power-up: ";
-                cin >> guess;
-
-                if (guess == 'R') {
-                    // Implement code for the reset power-up (reset attempts to 0)
-                    attempts = 0;
-                    cout << "Reset power-up used. Attempts reset to 0." << endl;
-                    continue;
-                } else if (guess == 'L') {
-                    // Implement code for the letter reveal power-up (reveal a letter in the word)
-                    // You can randomly choose a letter to reveal.
-                    int randomIndex = rand() % wordLength;
-                    guessed[randomIndex] = true;
-                    cout << "Letter reveal power-up used. A letter in the word is revealed." << endl;
-                    continue;
-                }
-            } else {
-                cout << ": ";
-                cin >> guess;
-            }
+            char guess = getValidatedInput();
 
             bool correctGuess = false;
             for (int i = 0; i < wordLength; i++) {
@@ -189,91 +208,13 @@ int main() {
                 attempts++;
             }
 
-            bool wordGuessed = true;
-            for (int i = 0; i < wordLength; i++) {
-                if (!guessed[i]) {
-                    wordGuessed = false;
-                    break;
+            if (attempts == maxAttempts - 1 && game_level != "hard") {
+                cout << "Would you like a hint? (y/n): ";
+                char hintOption;
+                cin >> hintOption;
+                if (hintOption == 'y' || hintOption == 'Y') {
+                    cout << "Hint: The word starts with " << provideHint(wordToGuess) << endl;
                 }
-            }
-
-            if (wordGuessed) {
-                int points = maxAttempts - attempts + 1; // Award points based on attempts left
-                score += points;
-                cout << "Congratulations! You guessed the word: " << wordToGuess << endl;
-                cout << "Points earned for this word: " << points << endl;
-                cout << "Total Score: " << score << endl;
-
-                // Ask if the player wants to go to the next level
-                cout << "Go to the next level? (y/n): ";
-                cin >> playAgain;
-                break;
-            }
-        }
-
-        if (attempts >= maxAttempts) {
-            cout << "Sorry, you've run out of attempts. The word was: " << wordToGuess << endl;
-            playAgain = 'n'; // Exit if the player fails to guess
-        }
-    } while (playAgain == 'y' || playAgain == 'Y');
-}
-else if(game_level=="medium"){
-	do {
-
-       
-            wordToGuess = chooseRandomWord(word_medium, numWords);
-        
-
-        int wordLength = wordToGuess.length();
-
-        for (int i = 0; i < wordLength; i++) {
-            guessed[i] = false;
-        }
-        int attempts = 0;
-
-        while (attempts < maxAttempts) {
-            cout << "Attempts remaining: " << maxAttempts - attempts << endl;
-            displayHangman(attempts);
-            displayWord(wordToGuess, guessed, wordLength);
-
-            char guess;
-            cout << "Guess a letter";
-
-            if (attempts == maxAttempts - 1) {
-                cout << " or enter 'R' for a reset power-up or 'L' for a letter reveal power-up: ";
-                cin >> guess;
-
-                if (guess == 'R') {
-                    // Implement code for the reset power-up (reset attempts to 0)
-                    attempts = 0;
-                    cout << "Reset power-up used. Attempts reset to 0." << endl;
-                    continue;
-                } else if (guess == 'L') {
-                    // Implement code for the letter reveal power-up (reveal a letter in the word)
-                    // You can randomly choose a letter to reveal.
-                    int randomIndex = rand() % wordLength;
-                    guessed[randomIndex] = true;
-                    cout << "Letter reveal power-up used. A letter in the word is revealed." << endl;
-                    continue;
-                }
-            } else {
-                cout << ": ";
-                cin >> guess;
-            }
-
-            bool correctGuess = false;
-            for (int i = 0; i < wordLength; i++) {
-                if (!guessed[i] && wordToGuess[i] == guess) {
-                    guessed[i] = true;
-                    correctGuess = true;
-                }
-            }
-
-            if (correctGuess) {
-                cout << "Correct guess!" << endl;
-            } else {
-                cout << "Incorrect guess." << endl;
-                attempts++;
             }
 
             bool wordGuessed = true;
@@ -285,29 +226,21 @@ else if(game_level=="medium"){
             }
 
             if (wordGuessed) {
-                int points = maxAttempts - attempts + 1; // Award points based on attempts left
-                score += points;
                 cout << "Congratulations! You guessed the word: " << wordToGuess << endl;
-                cout << "Points earned for this word: " << points << endl;
-                cout << "Total Score: " << score << endl;
-
-                // Ask if the player wants to go to the next level
-                cout << "Go to the next level? (y/n): ";
-                cin >> playAgain;
+                score++;
                 break;
             }
         }
 
-        if (attempts >= maxAttempts) {
-            cout << "Sorry, you've run out of attempts. The word was: " << wordToGuess << endl;
-            playAgain = 'n'; // Exit if the player fails to guess
+        if (attempts == maxAttempts) {
+            cout << "Game over! The word was: " << wordToGuess << endl;
         }
-    } while (playAgain == 'y' || playAgain == 'Y');
-}
-	
 
-    cout << "Thanks for playing word master! Final Score: " << score << endl;
+        cout << "Your score: " << score << endl;
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
 
     return 0;
 }
-
